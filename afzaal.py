88c4c6601ca118c23c279e40c8584f6b85e17a5f
mrhaxker <65105105+mrhaxker@users.noreply.github.com>
2020-05-11 12:04:27 +0500

    Update and rename aahil.py to afzaal.py

--- afzaal.py
+++ afzaal.py
@@ -4,8 +4,8 @@
         
         #############################################
         #                                           #
-        #    Facebook BruteForce, by Rana Aahil     #
-        #    Facebook Contact:       Aahil          #
+        #    Facebook BruteForce, by Mr Afzaal.     #
+        #    Facebook Contact:      Afzaal          #
         #                                           #
         #############################################
 
@@ -35,7 +35,7 @@ time.sleep(0.8)
 print '\n\nCracking '+user+' Now...'
 
 time.sleep(1)
-print '\nIM NOT RESPONSIBLE FOR ANY MISS USE AAHIL\n'
+print '\nIM NOT RESPONSIBLE FOR ANY MISS USE AFZAAL\n'
 for password in wordlist:
     if password == '' or password == ' ':
         pass
